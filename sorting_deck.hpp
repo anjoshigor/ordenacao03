bcfa25b831636f3521ae4d66fd359a682b574558
Higor Anjos <higor.araujo.anjos@gmail.com>
2016-08-22 22:40:17 -0300

    Algorithms added

---
+++ sorting_deck.hpp
@@ -0,0 +1,12 @@
+#ifndef SORTING_DECK_H
+#define SORTING_DECK_H
+
+namespace sorting_deck{
+  void insertion (int vet[], int n);
+  void selection (int vet[], int n);
+  void heap_sort(int vet[], int n);
+  void merge_sort(int vet[], int n);
+  void quicksort(int vet[], int n);
+}
+
+#endif
---
+++ src/quicksort.cpp
@@ -0,0 +1,31 @@
+#include "../sorting_deck.hpp"
+
+#include <utility>
+
+int partition(int vet[], int p, int r){
+  int x = vet[r];
+  int i = p;
+
+  for (int j = p; j < r; j++) {
+    if(vet[j] <= x){
+      std::swap(vet[i], vet[j]);
+      i++;
+    }
+  }
+
+  std::swap(vet[i], vet[r]);
+  return i;
+}
+
+void sort(int vet[], int p, int r){
+  int pivot;
+  if(p<r){
+    pivot = partition(vet, p, r);
+    sort(vet, p, pivot-1);
+    sort(vet, pivot+1, r);
+  }
+}
+
+void sorting_deck::quicksort(int vet[], int n){
+  sort(vet, 0, n-1);
+}
